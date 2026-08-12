6ada15efb61df4991cc6666c21abe49989841225
Tomisin2121 <bamideleisaac2106@gmail.com>
2026-08-12 15:54:49 +0100

    Changes

--- QuizApp/components/Start.jsx
+++ QuizApp/components/Start.jsx
@@ -1,8 +1,8 @@
 import './Start.css'
-export default function Start(){
+export default function Start({onStart}){
     return(
         <div className='start'>
-            <button className="start-button">Start</button>
+            <button className="start-button" onClick={onStart}>Start</button>
         </div>
     )
 }
--- QuizApp/src/App.jsx
+++ QuizApp/src/App.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import Start from '../components/Start'
 import Cards from '../components/Cards'
 import './App.css'
+import { quizData } from '../components/quizData.js'
 export default function App(){
+  const [currentIndex, setCurrentIndex]= React.useState(0)
+  const [score, setScore] = React.useState(0)
   const [gameState, setGameState] = React.useState('not-started')
+  function handleAnswer(isCorrect){
+    if(isCorrect){
+      setScore((prev)=> prev + 1)
+    }
+    setTimeout(() =>{
+      if(currentIndex + 1 < quizData.length){
+        setCurrentIndex((prev) => prev + 1)
+      } else {
+        setGameState('finished')
+      }
+    }, 1200)
+  }
   return(
 
     <div>
-      {gameState === 'not-started'&& <Start/>}
-      {gameState === 'in-progress'&& <Cards />}
+      {gameState === 'not-started'&& <Start onStart={() => setGameState('in-progress')} />}
+      {gameState === 'in-progress'&& <Cards questionData={quizData[currentIndex]}
+                                            onAnswer={handleAnswer} />}
     </div>
   )
 }
