a614e44b2b91cf5a6502a6d4ce04ca1d358332ab
Tomisin2121 <bamideleisaac2106@gmail.com>
2026-08-27 23:41:35 +0100

    Deletions

--- QuizApp/components/Start.jsx
+++
@@ -1,8 +0,0 @@
-import './Start.css'
-export default function Start({onStart}){
-    return(
-        <div className='start'>
-            <button className="start-button" onClick={onStart}>Start</button>
-        </div>
-    )
-}
